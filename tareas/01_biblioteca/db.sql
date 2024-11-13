b933837d2dc24c5c667e291a6e7f536ad1619545
jhordan1030 <jhordanhuera2002@gmail.com>
2024-11-13 17:17:04 -0500

    .sql subido

---
+++ tareas/01_biblioteca/db.sql
@@ -0,0 +1,11 @@
+create database biblioteca;
+GRANT ALL PRIVILEGES ON biblioteca.* TO 'jmhueran'@'localhost' IDENTIFIED BY 'jmhueran';
+create table libro(
+	lib_codigo int primary key,
+	lib_titulo varchar(50) not null,
+	lib_año int not null,
+	lib_autor varchar(50) not null,
+	lib_paginas int not null
+);
+insert into libro(lib_codigo,lib_titulo,lib_año,lib_autor,lib_paginas)values(1005,'Programando con PHP – básico.',2010,'John Carter',310);
+insert into libro(lib_codigo,lib_titulo,lib_año,lib_autor,lib_paginas)values(0401,'Desarrolle aplicaciones MVC en 15 días.',2013,'Hannibal Lecte',250);
