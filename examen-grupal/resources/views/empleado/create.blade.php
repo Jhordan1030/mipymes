1dc3fddbfcddba668e004ff0fba31b9f81da4984
Deysi705 <deysiguevara705@gmail.com>
2024-11-30 15:08:06 -0500

    Update create.blade.php de empleado

--- examen-grupal/resources/views/empleado/create.blade.php
+++ examen-grupal/resources/views/empleado/create.blade.php
@@ -1,1 +1,60 @@
+@extends('plantilla.plantilla')
+@section('content')
+<div class="row">
+    <section class="content">
+        <div class="col-md-8 col-md-offset-2">
+            @if (count($errors) > 0)
+            <div class="alert alert-danger">
+                <strong>Error!</strong> Revise los campos obligatorios.<br><br>
+                <ul>
+                    @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+            @endif
+            @if(Session::has('success'))
+            <div class="alert alert-info">
+                {{Session::get('success')}}
+            </div>
+            @endif
 
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h3 class="panel-title">Nuevo Cargo</h3>
+                </div>
+                <div class="panel-body">                    
+                    <div class="table-container">
+                        <form method="POST" action="{{ route('cargo.store') }}"  role="form">
+                            {{ csrf_field() }}
+                            <div class="row">
+                                <div class="col-xs-6 col-sm-6 col-md-6">
+                                    <div class="form-group">
+                                        <input type="text" name="codigo_cargo" id="codigo_cargo" class="form-control input-sm" placeholder="Codigo del cargo">
+                                    </div>
+                                </div>
+                              <div class="col-xs-6 col-sm-6 col-md-6">
+                                    <div class="form-group">
+                                        <input type="text" name="nombre_cargo" id="nombre_cargo" class="form-control input-sm" placeholder="Nombre del cargo">
+                                    </div>
+                                </div>
+                                </div>                                
+                            <div class="row">
+
+                                <div class="col-xs-12 col-sm-12 col-md-12">
+                                    <input type="submit"  value="Guardar" class="btn btn-success btn-block">
+                                    <a href="{{ route('cargo.index') }}" class="btn btn-info btn-block" >Atrás</a>
+                                </div>    
+
+                            </div>
+                        </form>
+                    </div>
+                </div>
+
+            </div>
+            <div class="footer"> 
+            <p>Creado por: Deysi Guevara</p> 
+          </div>
+        </div>
+    </section>
+    @endsection
