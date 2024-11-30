0f64f4e57ccd0ab097e93ef7bc8dc49eef6a48fd
Deysi705 <deysiguevara705@gmail.com>
2024-11-30 15:15:43 -0500

    Update create.blade.php de empleado

--- examen-grupal/resources/views/empleado/create.blade.php
+++ examen-grupal/resources/views/empleado/create.blade.php
@@ -21,29 +21,38 @@
 
             <div class="panel panel-default">
                 <div class="panel-heading">
-                    <h3 class="panel-title">Nuevo Cargo</h3>
+                    <h3 class="panel-title">Nuevo Empleado</h3>
                 </div>
                 <div class="panel-body">                    
                     <div class="table-container">
-                        <form method="POST" action="{{ route('cargo.store') }}"  role="form">
+                        <form method="POST" action="{{ route('empleado.store') }}"  role="form">
                             {{ csrf_field() }}
                             <div class="row">
                                 <div class="col-xs-6 col-sm-6 col-md-6">
                                     <div class="form-group">
-                                        <input type="text" name="codigo_cargo" id="codigo_cargo" class="form-control input-sm" placeholder="Codigo del cargo">
+                                        <input type="text" name="cedula_empleado" id="cedula_empleado" class="form-control input-sm" placeholder="Cédula del empleado">
                                     </div>
                                 </div>
                               <div class="col-xs-6 col-sm-6 col-md-6">
                                     <div class="form-group">
-                                        <input type="text" name="nombre_cargo" id="nombre_cargo" class="form-control input-sm" placeholder="Nombre del cargo">
+                                        <input type="text" name="telefono_empleado" id="telefono_empleado" class="form-control input-sm" placeholder="Teléfono del empleado">
                                     </div>
                                 </div>
-                                </div>                                
+                                </div>   
+                            <div class="form-group">
+                                        <input type="text" name="nombre_empleado" id="nombre_empleado" class="form-control input-sm" placeholder="Nombre del empleado">
+                                    </div>
+                            <div class="form-group">
+                                        <input type="text" name="apellidos_empleado" id="apellidos_empleado" class="form-control input-sm" placeholder="Apellidos del empleado">
+                                    </div>
+                            <div class="form-group">
+                                        <input type="text" name="direccion_empleado" id="direccion_empleado" class="form-control input-sm" placeholder="Dirección del empleado">
+                                    </div>
                             <div class="row">
 
                                 <div class="col-xs-12 col-sm-12 col-md-12">
                                     <input type="submit"  value="Guardar" class="btn btn-success btn-block">
-                                    <a href="{{ route('cargo.index') }}" class="btn btn-info btn-block" >Atrás</a>
+                                    <a href="{{ route('empleado.index') }}" class="btn btn-info btn-block" >Atrás</a>
                                 </div>    
 
                             </div>
