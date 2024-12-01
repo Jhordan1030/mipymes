24dc36afbc7eb91d36ae9e30abc0fb04d12861b9
Deysi <deysiguevara705@gmail.com>
2024-11-30 21:16:13 -0500

    Se agregó el boton a home en los index de tipo de cliente y tipo de identificación

--- examen-grupal/resources/views/tipoidentificacion/index.blade.php
+++ examen-grupal/resources/views/tipoidentificacion/index.blade.php
@@ -10,6 +10,7 @@
           <div class="pull-right">
             <div class="btn-group">
               <a href="{{ route('tipoidentificacion.create') }}" class="btn btn-info">Añadir Tipo de Identificación</a>
+              <a href="{{url('/home')}}" class="btn btn-primary" style="margin-left: 10px;">Home</a>
             </div>
           </div>
           <div class="table-container">
