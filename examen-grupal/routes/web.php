36657ee0988cc480a616171eeae13acc624b5a5c
Deysi <deysiguevara705@gmail.com>
2024-11-30 21:40:06 -0500

    Cambios en index cantón

--- examen-grupal/routes/web.php
+++ examen-grupal/routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\ParametroController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PaisController;
 use App\Http\Controllers\ProvinciaController;
@@ -9,10 +8,10 @@ use App\Http\Controllers\CargoController;
 use App\Http\Controllers\EmpleadoController;
 use App\Http\Controllers\TipoEmpaqueController;
 use App\Http\Controllers\TipoIdentificacionController;
-
 use App\Http\Controllers\TipoClienteController;
 use App\Http\Controllers\ProveedorController;
 use App\Http\Controllers\TipoPagoController;
+use App\Http\Controllers\ParametroController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -20,22 +19,14 @@ Route::get('/', function () {
 
 
 Route::resource('/parametro', ParametroController::class);
-Route::resource('/pais', PaisController::class);
+Route::resource('/tipo_empaque', TipoEmpaqueController::class);
+Route::resource('/tipoidentificacion', TipoIdentificacionController::class);
+Route::resource('tipocliente', TipoClienteController::class);
 Route::resource('/provincia', ProvinciaController::class);
 Route::resource('/canton', CantonController::class);
-Route::resource('/tipo_empaque', TipoEmpaqueController::class);
-
-
-Route::resource('/tipoidentificacion', TipoIdentificacionController::class);
-
-
-
-
-Route::resource('tipocliente', TipoClienteController::class);
-Route::resource('/canton',CantonController::class);
 Route::resource('/empleado', EmpleadoController::class);
 Route::resource('/cargo',CargoController::class);
-Route::resource('/tipo_empaque', TipoEmpaqueController::class);
+Route::resource('/pais', PaisController::class);
 Route::resource('/proveedor', ProveedorController::class);
 Route::resource('/tpago', TipoPagoController::class);
 
