7be88b5a6c1657cf30e98cac301ebb2315629224
Marcheloo-7 <marcelochiriboga246@gmail.com>
2025-01-14 07:13:47 -0500

    tipo de nota

--- inventario_anturios/resources/views/tipoNota/create.blade.php
+++ inventario_anturios/resources/views/tipoNota/create.blade.php
@@ -1,4 +1,4 @@
-@extends('layout.app')
+@extends('layouts.app')
 @section('content')
 <div class="container">
     <h3>Crear Nuevo Tipo de Nota</h3>
--- inventario_anturios/resources/views/tipoNota/index.blade.php
+++ inventario_anturios/resources/views/tipoNota/index.blade.php
@@ -1,4 +1,4 @@
-@extends('layout.app')
+@extends('layouts.app')
 @section('content')
 <div class="container">
     <h3>Lista de Notas</h3>
